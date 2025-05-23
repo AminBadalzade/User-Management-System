e1dbad1a35ac9fe58cbcf8b1cb8d674093ebdcba
AminBadalzade <amin.badalzade@sa.stud.vu.lt>
2025-05-23 17:06:44 +0300

    Adding Encryption for signup system

--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -1,40 +1,43 @@
 <?php
 
-class Signup extends Dbh{
-    protected function setUser($uid, $pwd, $email){
-        $stmt = $this->connect()->prepare("INSERT INTO users (users_uid, users_pwd, users_email) VALUES (?,?,?);"); 
-    
-        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-    
-    if(!$stmt -> execute(array($uid,$hashedPwd,$email))){
+class Signup extends Dbh {
+
+    private function aesEncrypt($data, $key) {
+        $cipher = "AES-256-CBC";
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
+        $encrypted = openssl_encrypt($data, $cipher, $key, 0, $iv);
+        return base64_encode($iv . $encrypted);
+    }
+
+    protected function setUser($uid, $pwd, $email) {
+        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT); 
+        $aesKey = hash('sha256', $pwd, true);                
+        $encryptedPwd = $this->aesEncrypt($pwd, $aesKey);    
+
+        $stmt = $this->connect()->prepare(
+            "INSERT INTO users (users_uid, users_pwd, users_key, users_email) VALUES (?, ?, ?, ?);"
+        );
+
+        if (!$stmt->execute([$uid, $hashedPwd, $encryptedPwd, $email])) {
+            $stmt = null;
+            header("location: ../index.php?error=stmtfailed");
+            exit();
+        }
+
         $stmt = null;
-        header("location: ../index.php?error=stmtfailed");
-        exit();
     }
 
-    $stmt = null;
-    }
+    protected function checkUser($uid, $email) {
+        $stmt = $this->connect()->prepare(
+            "SELECT users_uid FROM users WHERE users_uid = ? OR users_email = ?;"
+        );
 
+        if (!$stmt->execute([$uid, $email])) {
+            $stmt = null;
+            header("location: ../index.php?error=stmtfailed");
+            exit();
+        }
 
-
-
-    protected function checkUser($uid, $email){
-         $stmt = $this->connect()->prepare("SELECT users_uid FROM users WHERE users_uid = ? OR users_email = ?;"); 
-    if(!$stmt -> execute(array($uid,$email))){
-        $stmt = null;
-        header("location: ../index.php?error=stmtfailed");
-        exit();
-    }
-
-    $resultCheck;
-
-    if($stmt->rowCount() > 0){
-    $resultCheck = false;
-    }
-    else{
-        $resultCheck = true;
-    }
-
-    return $resultCheck; 
+        return $stmt->rowCount() === 0;
     }
 }
